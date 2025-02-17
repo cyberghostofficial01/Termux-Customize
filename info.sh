b55de8ef0101dfaa80e6f13e86c19f395005b837
CYBER GHOST <cyberghostofficial01@gmail.com>
2025-02-17 11:29:22 +0600

    Update info.sh

--- info.sh
+++ info.sh
@@ -10,7 +10,7 @@ echo -e "\033[32m\033[1m
   \033[31mWarning:\033[37mDon't Forget Your \033[35mUsernames \033[37mand \033[35mPassword
   \033[33m________________________________________________
 
-     \033[1m\033[36mTool Name       \033[32m: \033[33mTermux Login
+     \033[1m\033[36mTool Name       \033[32m: \033[33mTermux-Customize
      \033[1m\033[36mAuthor_Name     \033[32m: \033[33mCYBER GHOST 
      \033[1m\033[36mYouTube_Channel \033[32m: \033[33m@cyberghostofficial01  
      \033[1m\033[36mCountry         \033[32m: \033[33mBangladesh  
