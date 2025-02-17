384213c1f987ab62484083c0075bd0c32f461e8a
CYBER GHOST <cyberghostofficial01@gmail.com>
2025-02-17 18:32:13 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ clear
 echo
 pkg install pv -y >/dev/null 2>&1
 echo -e "\033[32m\033[1m]────────────────────────────────────────────["
-echo -e "\033[33m\033[1m   Installing Packages and All Dependencies" | pv -qL 10
+echo -e "\033[33m\033[1m   Installing Packages and All Dependencies"
 apt update                    
 apt upgrade -y 
 pkg install python -y 
@@ -13,7 +13,7 @@ pkg install python3 -y
 
 pkg install termux-api -y 
 termux-setup-storage
-echo -e "\033[31m\033[1m        INSTALLED SUCCESSFULLY \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[31m\033[1m        INSTALLED SUCCESSFULLY \033[32m[\033[36m✓\033[32m]"
 echo -e "\033[33m\033[1m]────────────────────────────────────────────["
 
 
